786529a6560e8d98a9828a30d5958bfe39076b3f
Jarito404 <jamesvtende@gmail.com>
2023-10-04 13:06:05 +0200

    opdracht 3 af

--- w6.php
+++ w6.php
@@ -5,15 +5,17 @@ $choice = trim(fgets(STDIN));
 switch($choice) {//case-switch is best wel similar aan allemaal if-statements
     //opdracht 1
     case 1:
-        //a - c
+        //a
         $klassenlist = array("tobias", "hasna", "aukje", "fred");
         print_r($klassenlist);
 
+        //b
         print("\n");
         for($b = 0; $b < count($klassenlist); $b++) {//print alle elementen uit in de array
             print($b . ": " . $klassenlist[$b] . "\n");
         }
 
+        //c
         sort($klassenlist);
         print("\n");
 
@@ -22,6 +24,39 @@ switch($choice) {//case-switch is best wel similar aan allemaal if-statements
         }
         break;
 
+    //opdracht 2
+    case 2:
+        $playlist = array();
+        $aantalNummers = 3;
+
+        for($h2 = 0; $h2 < $aantalNummers; $h2++) {
+            print ("nummer ". ($h2 + 1) . ": \n");
+            $playlist[$h2] = trim(fgets(STDIN));
+        }
+
+        print_r($playlist);
+        break;
+
+    //opdracht 3
+    case 3:
+        $rondeTijd = array(5,5,6,6,7);
+
+        for($h3 = 0; $h3 < count($rondeTijd); $h3++) {
+            print ("ronde ". ($h3 + 1) . ": " . $rondeTijd[$h3] . " minuten\n");
+        }
+
+        $aantalRonden = count($rondeTijd);
+        print("aantal ronden: " . $aantalRonden . "\n");
+
+        print("totale tijd: " . array_sum($rondeTijd) . " minuten\n");
+        print("snelste ronde: " . min($rondeTijd) . " minuten\n");
+        print("langzaamste ronde: " . max($rondeTijd) . " minuten\n");
+        break;
+
+    //opdracht 4
+    case 4:
+        
+        break;
     //als de input anders is dan een nummer van een opdracht
     default:
         print("Bestaat niet");
