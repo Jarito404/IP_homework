4b4681aca60bc00732c9e64aa37810944579cb67
Jarito404 <jamesvtende@gmail.com>
2023-10-04 12:15:31 +0200

    opdracht 1 af

--- w6.php
+++ w6.php
@@ -5,11 +5,21 @@ $choice = trim(fgets(STDIN));
 switch($choice) {//case-switch is best wel similar aan allemaal if-statements
     //opdracht 1
     case 1:
-        //a
+        //a - c
         $klassenlist = array("tobias", "hasna", "aukje", "fred");
         print_r($klassenlist);
 
-        
+        print("\n");
+        for($b = 0; $b < count($klassenlist); $b++) {//print alle elementen uit in de array
+            print($b . ": " . $klassenlist[$b] . "\n");
+        }
+
+        sort($klassenlist);
+        print("\n");
+
+        for($b = 0; $b < count($klassenlist); $b++) {
+            print($b . ": " . $klassenlist[$b] . "\n");
+        }
         break;
 
     //als de input anders is dan een nummer van een opdracht
