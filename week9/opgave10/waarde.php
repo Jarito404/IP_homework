75899bc3f9128c843248d7ad51681ce00be3f578
Jarito404 <jamesvtende@gmail.com>
2023-10-14 17:13:10 +0200

    w9 opgave 10 af

--- week9/opgave10/waarde.php
+++ week9/opgave10/waarde.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-if($_POST != NULL)
-    $_SESSION["waardes"][] = $_POST["waarde"];
+if($_POST != NULL) {
+    if ($_POST["waarde"] > 0 && $_POST["waarde"] <= 5)
+        $_SESSION["waardes"][] = $_POST["waarde"];
+    else
+        print("Verkeerde waarde! (" . $_POST["waarde"]. ")<br>");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,8 +14,16 @@ if($_POST != NULL)
     <title>Title</title>
 </head>
 <body>
-<?php print_r($_SESSION);?>
-<form method="post" action="check.php">
+<?php
+if(isset($_SESSION["waardes"])) {
+    foreach($_SESSION["waardes"] as $waarde) {
+        for($i = 0;$i < $waarde; $i++) {
+            print("*");
+        }
+        print("<br>");
+    }
+}?>
+<form method="post" action="waarde.php">
     <label for="waarde">
         Geef een waarde:<br>
         <input id="waarde" type="number" name="waarde">
